e8d2691a23d66b7f6517e3a82feb7ece53f78419
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2023-12-21 07:36:05 +0530

    Setting up React with Vite
    Initial commit and setting up files

---
+++ single page developer portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Components/Header.jsx";
+import Main from "./Components/Main.jsx";
+import Footer from "./Components/Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
