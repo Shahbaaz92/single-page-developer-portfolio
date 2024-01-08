fda2340c37c1093db3400d596a28773ab4332f44
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2024-01-08 16:18:48 +0530

    Update Modal.jsx

--- single page developer portfolio/src/Components/Modal.jsx
+++ single page developer portfolio/src/Components/Modal.jsx
@@ -1,6 +1,6 @@
-function Modal({ setOpenModal }) {
+function Modal(props) {
   return (
-    <div className="remodal" onClick={() => setOpenModal(false)}>
+    <div className="remodal" onClick={() => props.setOpenModal(false)}>
       <button>View Project</button>
       <button>View Code</button>
     </div>
